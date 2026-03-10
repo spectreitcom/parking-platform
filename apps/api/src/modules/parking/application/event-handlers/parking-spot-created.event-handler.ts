a2ddde59a2ffe1e3c66d42307988d7514a3b6dc4
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-03-10 14:21:06 +0100

    feat: update parking spot created handler to increment parkingSpotsNumber in admin list

--- apps/api/src/modules/parking/application/event-handlers/parking-spot-created.event-handler.ts
+++ apps/api/src/modules/parking/application/event-handlers/parking-spot-created.event-handler.ts
@@ -1,12 +1,27 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { Logger } from '@nestjs/common';
 import { ParkingSpotCreatedEvent } from '../../domain/events/parking-spot-created.event';
+import { PrismaService } from '../../../../shared/prisma/prisma.service';
 
 @EventsHandler(ParkingSpotCreatedEvent)
 export class ParkingSpotCreatedEventHandler implements IEventHandler<ParkingSpotCreatedEvent> {
   private readonly logger = new Logger(ParkingSpotCreatedEventHandler.name);
 
-  handle(event: ParkingSpotCreatedEvent) {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async handle(event: ParkingSpotCreatedEvent) {
     this.logger.log(`Parking spot created: ${event.id}`);
+    const { parkingId } = event;
+
+    await this.prismaService.parkingListForAdminRead.updateMany({
+      where: {
+        parkingId,
+      },
+      data: {
+        parkingSpotsNumber: {
+          increment: 1,
+        },
+      },
+    });
   }
 }
