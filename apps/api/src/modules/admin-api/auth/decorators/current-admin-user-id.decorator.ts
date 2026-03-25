6fe396bce2b043d0c64826a6a5489b5ac16ba1d8
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-03-25 14:38:16 +0100

    feat: add validation for CurrentAdminUserId decorator to handle missing user scenario

--- apps/api/src/modules/admin-api/auth/decorators/current-admin-user-id.decorator.ts
+++ apps/api/src/modules/admin-api/auth/decorators/current-admin-user-id.decorator.ts
@@ -1,10 +1,16 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, UnauthorizedException } from '@nestjs/common';
 import { Request } from 'express';
 import { RequestUser } from 'src/modules/admin-api/auth/types';
 
 export const CurrentAdminUserId = createParamDecorator<void, string>(
   (_, executionContext) => {
     const request: Request = executionContext.switchToHttp().getRequest();
-    return (request.user as RequestUser).id;
+    const user = request.user as RequestUser | undefined;
+
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+
+    return user.id;
   },
 );
