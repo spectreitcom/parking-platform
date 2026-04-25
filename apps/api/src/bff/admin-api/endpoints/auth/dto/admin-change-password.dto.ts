ca466e7ec7732982770f33f4c6fe22c195b87b5c
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-04-25 11:24:14 +0200

    refactor: rename DTOs for admin and user auth modules to improve clarity and consistency

--- apps/api/src/bff/admin-api/endpoints/auth/dto/admin-change-password.dto.ts
+++ apps/api/src/bff/admin-api/endpoints/auth/dto/admin-change-password.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class ChangePasswordDto {
+export class AdminChangePasswordDto {
   @ApiProperty({
     description: 'The existing password',
     required: true,
--- apps/api/src/bff/api/endpoints/auth/dto/user-reset-password-token.dto.ts
+++ apps/api/src/bff/api/endpoints/auth/dto/user-reset-password-token.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-export class ResetPasswordTokenDto {
+export class UserResetPasswordTokenDto {
   @ApiProperty({
     description: 'The reset password token',
     required: true,
